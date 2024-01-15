e29b771f7c46d8bbc8f67cbe394d3e88c1b205e7
mandel2000 <mandelsan2000@gmail.com>
2024-01-15 18:31:48 +0100

    Now showing matches in an ion-list

--- src/app/components/match/match.component.ts
+++ src/app/components/match/match.component.ts
@@ -18,7 +18,7 @@ export class MatchComponent  implements OnInit {
   getDate(){
 
     let date = new Date(this.match.fixture.date);
-    return date.getDate().toString().padStart(2, '0') + ' - ' + (date.getMonth()+1).toString().padStart(2, '0') + ' - ' + date.getFullYear();
+    return date.getDate().toString().padStart(2, '0') + '/' + (date.getMonth()+1).toString().padStart(2, '0') + '/' + date.getFullYear();
   }
 
   getTime(){
